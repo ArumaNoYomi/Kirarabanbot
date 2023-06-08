b3df1db167d423fb86d174859e510d279a171cdd
Альма Мелихова <134545326+ArumaNoYomi@users.noreply.github.com>
2023-06-08 03:41:13 +0300

    Update1 main.py

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ async def check2(message: types.Message):
 
     await message.reply("Голосование") # добавить войсом голосование мем
 
-    buttonyes = InlineKeyboardButton("Да", callback_data='Yes')
+    pidor_fullname = message.reply_to_message.from_user.full_name
+
+    buttonyes = InlineKeyboardButton("Да", callback_data='Yes.'+ pidor_fullname)
     buttonno = InlineKeyboardButton("Нет", callback_data='No')
     buttons = InlineKeyboardMarkup()
     buttons.add(buttonyes)
@@ -26,15 +28,14 @@ async def check2(message: types.Message):
     if message.reply_to_message == None:
         return  # Выход из функции, else не нужон
 
-    pidor_id = message.reply_to_message.from_id
-    pidor_fullname = message.reply_to_message.from_user.full_name
-    await message.reply(f'Проголосовать за бан этого пидораса {pidor_fullname} ?', reply_markup=buttons)\
+    await message.reply(f'Проголосовать за бан этого пидораса {pidor_fullname} ?', reply_markup=buttons)
 
-@dp.callback_query_handler(lambda c: c.data == 'buttonyes')
-async def process_callback_buttonyes(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, f'Проголосовать за бан этого пидораса {pidor_fullname} ? 1 человек проголосовал!')
-
+    @dp.callback_query_handler(lambda c: 'Yes' in c.data)
+    async def process_callback_buttonyes(callback_query: types.CallbackQuery):
+        pidor_fullname = callback_query.data.split(".")[1]
+        if process_callback_buttonyes == process_callback_buttonyes:
+            await callback_query.message.edit_text(f'Проголосовать за бан этого пидораса {pidor_fullname} ? 1 человек проголосовал!', reply_markup=buttons)
+            
 
 
 
