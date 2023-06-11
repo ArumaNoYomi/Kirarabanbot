9bc3e768680c2b465a7c29596e8c8040101fa504
Альма Мелихова <134545326+ArumaNoYomi@users.noreply.github.com>
2023-06-11 22:43:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ async def check(message: types.Message):
 @dp.message_handler(commands=["voteban"])
 async def check2(message: types.Message):
 
-    await message.reply("Голосование") # добавить войсом голосование мем
 
     pidor_fullname = message.reply_to_message.from_user.full_name
+    pidorid = message.reply_to_message.from_user.id
 
-    buttonyes = InlineKeyboardButton("Да", callback_data='Yes.'+ pidor_fullname)
+    buttonyes = InlineKeyboardButton("Да", callback_data='Yes.'+ pidor_fullname +"."+ str(pidorid))
     buttonno = InlineKeyboardButton("Нет", callback_data='No')
     buttons = InlineKeyboardMarkup()
     buttons.add(buttonyes)
@@ -31,17 +31,39 @@ async def check2(message: types.Message):
         return  # Выход из функции, else не нужон
 
     await message.reply(f'Проголосовать за бан этого пидораса {pidor_fullname} ?', reply_markup=buttons)
+    with open("golosovanie.opus", mode="rb") as file:
+        binary_content = file.read()
+    await message.reply_voice(binary_content, reply=False)
 
 @dp.callback_query_handler(lambda c: 'Yes' in c.data)
 async def process_callback_buttonyes(callback_query: types.CallbackQuery):
     pidor_fullname = callback_query.data.split(".")[1]
-    # await callback_query.message.edit_text(f'Проголосовать за бан этого пидораса {pidor_fullname} ? 1 человек проголосовал!', reply_markup=buttons)
-    if (callback_query.message.chat.id, callback_query.message.message_id) in database:
-        database[(callback_query.message.chat.id, callback_query.message.message_id)].add(callback_query.from_user.id)
+
+
+    pidorid = int(callback_query.data.split(".")[2])
+    key = callback_query.message.chat.id, callback_query.message.message_id
+
+    if (key) in database:
+        database[(key)].add(callback_query.from_user.id)
     else:
-        database[(callback_query.message.chat.id, callback_query.message.message_id)] = set([callback_query.from_user.id])
+        database[(key)] = set([callback_query.from_user.id])
     await callback_query.answer()
+
+    buttonyes = InlineKeyboardButton("Да", callback_data='Yes.' + pidor_fullname + "." + str(pidorid))
+    buttonno = InlineKeyboardButton("Нет", callback_data='No')
+    buttons = InlineKeyboardMarkup()
+    buttons.add(buttonyes)
+    buttons.add(buttonno)
+
     for key in database:
-        counter = (len(database[key]))
-#        if counter == 7:
-#           await
+        counter = len(database[key])
+        if counter == 10:
+            await bot.ban_chat_member(callback_query.message.chat.id, pidorid)
+            await callback_query.message.edit_text(f'Проголосовать за бан этого пидораса {pidor_fullname} ? Пидор забанен!', reply_markup=buttons)
+        else:
+            try:
+                await callback_query.message.edit_text(f'Проголосовать за бан этого пидораса {pidor_fullname} ? {counter}', reply_markup=buttons)
+            except Exception as e:
+                pass
+
+executor.start_polling(dp, skip_updates=True)
